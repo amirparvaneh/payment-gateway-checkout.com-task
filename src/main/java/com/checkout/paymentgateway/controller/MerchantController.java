71b42a7c6fc35278db6f2230f687b14aa21cb180
amirparvaneh <amirparvaneh138@gmail.com>
2023-06-19 19:39:50 +0330

    process-payment

---
+++ src/main/java/com/checkout/paymentgateway/controller/MerchantController.java
@@ -0,0 +1,12 @@
+package com.checkout.paymentgateway.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = "/merchant")
+public class MerchantController {
+
+
+}
